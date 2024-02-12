3861d93a8ccccb80ba71632e8a6fad82cd8fbd44
Sudha Ramanantham <132622868+SudhaRamanantham@users.noreply.github.com>
2024-02-12 10:44:36 -0800

    created a method on branch Sudha

--- src/java1/JavaDay1.java
+++ src/java1/JavaDay1.java
@@ -2,4 +2,13 @@ package java1;
 
 public class JavaDay1 {
 
+	public static void sudha() {
+		// TODO Auto-generated method stub
+
+		System.out.println("sudha");
+	}
+	public static void main(String[] args) {
+		sudha();
+	}
+ 	
 }
