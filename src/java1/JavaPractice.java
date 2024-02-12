42f30f57d79fe6cfa95882984ddef137db650186
DeepaIGH <ideepali346@gmail.com>
2024-02-12 13:54:22 -0500

    Created new class by Deepali.

---
+++ src/java1/JavaPractice.java
@@ -0,0 +1,10 @@
+package java1;
+
+public class JavaPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello There. My first trial on creating new branch.");
+	}
+
+}
